2c415a4d3cf40ab5ed3afc505ea7e1d459e96cc9
Dzovani Koller <dzovani.koller@outlook.de>
2021-09-01 17:13:53 +0200

    fixed skills bar
    fixed skill bar

--- src/components/portfolio/portfolio.jsx
+++ src/components/portfolio/portfolio.jsx
@@ -9,55 +9,57 @@ export default function Portfolio() {
   }
   
   return <div className="portfolio" id="skills">
+   <p className="p">Skills</p>
+    <div className="FlexBox">
      {newFunction("Programming Language","Javascript","Typescript","Java","C#","CC","Python")}
-     {newFunction("Programming Language","Javascript","Typescript","Java","C#","CC","Python")}
-     {newFunction("Programming Language","Javascript","Typescript","Java","C#","CC","Python")}
+     {newFunction("Design Language","Javascript","Typescript","Java","C#","CC","Python")}
+     </div>
   </div>;
   function newFunction(title,first,second,third,fourth,fourth2,fifth) {
-    return <div class="skill-bars">
+    return <div className="skill-bars">
       <p className="p5" ref={ref}>{title}</p>
-      <div class="bar">
-        <div class="info">
+      <div className="bar">
+        <div className="info">
           <span>{first}</span>
         </div>
-        <div class={"progress-line "+first}>
+        <div className={"progress-line "+first}>
           <span></span>
         </div>
       </div>
-      <div class="bar">
-        <div class="info">
+      <div className="bar">
+        <div className="info">
           <span>{second}</span>
         </div>
-        <div class={"progress-line "+second}>
+        <div className={"progress-line "+second}>
           <span></span>
         </div>
       </div>
       {third && 
-      <div class="bar">
-        <div class="info">
+      <div className="bar">
+        <div className="info">
           <span>{third}</span>
         </div>
-        <div class={"progress-line "+third}>
+        <div className={"progress-line "+third}>
           <span></span>
         </div>
       </div>
       }
       {fourth && 
-      <div class="bar">
-        <div class="info">
+      <div className="bar">
+        <div className="info">
           <span>{fourth}</span>
         </div>
-        <div class={"progress-line "+fourth2}>
+        <div className={"progress-line "+fourth2}>
           <span></span>
         </div>
       </div>
       }
        {fifth && 
-      <div class="bar">
-        <div class="info">
+      <div className="bar">
+        <div className="info">
           <span>{fifth}</span>
         </div>
-        <div class={"progress-line "+fifth}>
+        <div className={"progress-line "+fifth}>
           <span></span>
         </div>
       </div>
